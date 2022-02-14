2c290c78f0650b3eefa392af55a8214990e98553
pdeka <prabindeka@gmail.com>
2022-02-14 11:50:08 +1100

    transactions

--- xero_app.rb
+++ xero_app.rb
@@ -134,3 +134,17 @@ get '/reports' do
   @reports = xero_client.accounting_api.get_report_ba_sor_gst_list(xero_client.connections[0]['tenantId']).reports
   haml :reports
 end
+
+get '/transactions' do
+  xero_client.set_token_set(session[:token_set])
+
+  xero_tenant_id = xero_client.connections[0]['tenantId']
+  if_modified_since = "2020-02-06T12:17:43.202-08:00"
+  where = 'Status==#{XeroRuby::Accounting::BankTransaction::AUTHORISED}'
+  order = 'Type ASC'
+  page = 1
+  unitdp = 4
+
+  @transactions = xero_client.accounting_api.get_bank_transactions(xero_tenant_id, if_modified_since, where, order, page, unitdp)
+  haml :transactions
+end
